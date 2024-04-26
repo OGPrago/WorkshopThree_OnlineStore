c05de3f017645deb86f262c79bb476e9fd5c6723
Jeremy <ogprago@gmail.com>
2024-04-26 14:29:28 -0400

    Created Home Scren

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,30 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int userIn;
+
+        do {
+            System.out.println("Welcome to the store. Please select an option: ");
+            System.out.println("\t1) Display Products");
+            System.out.println("\t2) Display Cart");
+            System.out.println("\t3) Exit");
+            userIn = scanner.nextInt();
+
+            switch (userIn) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Command not found");
+            }
+
+        } while(userIn != 3);
     }
 }
