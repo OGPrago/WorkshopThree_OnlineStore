e1aa5260b527d2e75a9c17e1ec500e109c7c46ff
tmoore8 <107230156+tmoore8@users.noreply.github.com>
2024-04-26 15:21:26 -0400

    added ShoppingCart and Store Classes with associated methods

---
+++ src/main/java/com/ps/ShoppingCart.java
@@ -0,0 +1,20 @@
+package com.ps;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    //ArrayList containing items in cart
+    private ArrayList<Product> itemsInCart;
+    //Constructor of Shopping Cart
+    public ShoppingCart(){
+        itemsInCart = new ArrayList<>();
+    }
+    //method to add a product to cart
+    public void addItem(Product product){
+        itemsInCart.add(product);
+    }
+    //method to read list of items in cart
+    public ArrayList<Product> getItemsInCart(){
+        return itemsInCart;
+    }
+}
