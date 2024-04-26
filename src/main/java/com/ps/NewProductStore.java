85305b5a46546d9698cb3efd2952665b6cf98af5
tmoore8 <107230156+tmoore8@users.noreply.github.com>
2024-04-26 16:06:20 -0400

    added  toString

---
+++ src/main/java/com/ps/NewProductStore.java
@@ -0,0 +1,21 @@
+package com.ps;
+
+public class NewProductStore {
+    public List<JBL Bluetooth
+    Speaker>
+
+    readProductFromCSV("New Product") {
+        List<JBL Bluetooth Speaker > products = New ArrayList<> ;
+
+        public Array<Product> returnItemsInCart () {
+            return ItemsinCart;
+
+        }
+
+       public Array<Product> (cancelOrderInCart) {
+                delete ItemsinCart;
+
+       }
+
+    }
+}
--- src/main/java/com/ps/ShoppingCart.java
+++ src/main/java/com/ps/ShoppingCart.java
@@ -1,6 +1,7 @@
 package com.ps;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ShoppingCart {
     //ArrayList containing items in cart
@@ -18,3 +19,25 @@ public class ShoppingCart {
         return itemsInCart;
     }
 }
+// Steps to add items to a cart
+//1.Go to store website
+//2. Select item
+//3. Click to add to cart
+//4.Click go to cart
+//5.Select go to check out, remove item, edit quantity, compare with other items, share
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userIn;
+
+        do {
+            System.out.println("Please select an item ");
+            System.out.println("\t1) Add to Cart");
+            System.out.println("\t2) Display Cart");
+            System.out.println("\t3) Edit quantity of items");
+            System.out.println("\t4)  Remove items");
+            System.out.println("\t5) Find similar items");
